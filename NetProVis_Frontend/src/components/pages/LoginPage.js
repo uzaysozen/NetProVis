cb531e9cfca6201985c643b8639003074af6c1a6
Uzay Sozen <uzaysozen@gmail.com>
2023-09-15 02:30:56 +0300

    Changed Login UI

--- NetProVis_Frontend/src/components/pages/LoginPage.js
+++ NetProVis_Frontend/src/components/pages/LoginPage.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import {Card} from 'antd';
-import GoogleButton from "react-google-button";
-import LogoImage from "../../assets/NetProVis_Logo.png"; // Replace 'logo.png' with your actual logo file
+import { Card } from 'antd';
+import GoogleButton from 'react-google-button';
+import LogoImage from '../../assets/NetProVis_Logo.png';
 import '../../styles/LoginPage.css';
 
-const LoginPage = ({signIn}) => {
+const LoginPage = ({ signIn }) => {
     return (
         <div className="login-container">
             <Card className="login-card">
-                <img src={LogoImage} alt="Logo" className="logo-image" width={150} height={150}/>
-                <h1 className="login-title">NetProVis</h1>
-                <h3 className="login-subtitle">A k8s cluster provisioning and management tool</h3>
+                <img src={LogoImage} alt="Logo" className="logo-image" width={200} height={200} />
+                <h1 className="login-title">Welcome to NetProVis</h1>
+                <h3 className="login-subtitle">A Kubernetes Cluster Provisioning and Management Tool</h3>
                 <div className="button-container">
-                    <GoogleButton className="google-button" onClick={signIn}>
+                    <GoogleButton className="google-button" type="dark" onClick={signIn}>
                         Sign In with Google
                     </GoogleButton>
                 </div>
